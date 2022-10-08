39dd32611b47722997f697ef80e4e9cb3874cd77
Harry Sheppard <harry.99lives@pm.me>
2022-10-08 16:30:14 +1100

    Updated to support new seperate training and testing files

--- data/bdata.py
+++ data/bdata.py
@@ -5,32 +5,39 @@ import numpy as np  # Provides arrays, matrixies, and maths functions
 import pandas as pd # Data analysis and manipulation
 
 
-def process_data(data):
+def process_data(train, test):
     # pandas DataFrame of boroondara data
-    boroondara_df = pd.read_csv(data, encoding='utf-8').fillna(0)
+    train_df = pd.read_csv(train, encoding='utf-8').fillna(0)
+    test_df  = pd.read_csv(test,  encoding='utf-8').fillna(0)
 
     # Drop columns that are irrelevant to training the model
-    boroondara_df = boroondara_df.drop(['CD_MELWAY' ,'NB_LATITUDE', 'NB_LONGITUDE', 'HF VicRoads Internal', 'VR Internal Stat', 'VR Internal Loc', 'NB_TYPE_SURVEY'], axis='columns')
+    train_df = train_df.drop(['CD_MELWAY' ,'NB_LATITUDE', 'NB_LONGITUDE', 'HF VicRoads Internal', 'VR Internal Stat', 'VR Internal Loc', 'NB_TYPE_SURVEY'], axis='columns')
+    test_df = test_df.drop(['CD_MELWAY' ,'NB_LATITUDE', 'NB_LONGITUDE', 'HF VicRoads Internal', 'VR Internal Stat', 'VR Internal Loc', 'NB_TYPE_SURVEY'], axis='columns')
 
     # Convert columns of values at times into rows
-    boroondara_df = pd.melt(boroondara_df,
+    train_df = pd.melt(train_df,
+            id_vars=['SCATS Number', 'Location', 'Start Time'],
+            var_name='Time', value_name='Traffic')
+    test_df = pd.melt(test_df,
             id_vars=['SCATS Number', 'Location', 'Start Time'],
             var_name='Time', value_name='Traffic')
 
     # Sort rows
-    boroondara_df = boroondara_df.sort_values(by=['SCATS Number', 'Location', 'Start Time'])
+    train_df = train_df.sort_values(by=['SCATS Number', 'Location', 'Start Time'])
+    test_df = test_df.sort_values(by=['SCATS Number', 'Location', 'Start Time'])
     # Reorder the index
-    boroondara_df = boroondara_df.reset_index(drop=True)
+    train_df = train_df.reset_index(drop=True)
+    test_df = test_df.reset_index(drop=True)
     # Scale the traffic values
-    boroondara_df['Traffic'] = (
-            boroondara_df['Traffic'] - boroondara_df['Traffic'].min()) / \
-            (boroondara_df['Traffic'].max()-boroondara_df['Traffic'].min()
+    train_df['Traffic'] = (
+            train_df['Traffic'] - train_df['Traffic'].min()) / \
+            (train_df['Traffic'].max()-train_df['Traffic'].min()
     )
-
-    # Splt into training and testing data
-    train_df = boroondara_df[boroondara_df.index % 8 != 0]
-    test_df  = boroondara_df[boroondara_df.index % 8 == 0]
+    test_df['Traffic'] = (
+            test_df['Traffic'] - test_df['Traffic'].min()) / \
+            (test_df['Traffic'].max()-test_df['Traffic'].min()
+    )
 
 
 if __name__ == '__main__':
-    process_data("data/boroondara.csv")
+    process_data('data/boroondaraTrain.csv', 'data/boroondaraTest.csv')
