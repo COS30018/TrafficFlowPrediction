d28bed2b3c28fe75cb09411346dd4db24fd2845f
ChristianDeGroot <104880638+ChristianDeGroot@users.noreply.github.com>
2022-10-11 08:40:36 +1100

    test

--- data/data.py
+++ data/data.py
@@ -26,21 +26,62 @@ def process_data(train="train.csv", test="test.csv", lags=12):
     
 
     df1 = pd.read_csv(train, encoding='utf-8').fillna(0)
-    print("test")
-    print(df1)
-    print("printing df1[attr]")
-    print(df1[attr])
+    #print("test")
+    #print(df1)
+    #print("printing df1[attr]")
+    #print(df1[attr])
 
-    names = ["SCATS Number", "Location", "CD_MELWAY", "NB_LATITUDE", "NB_LONGITUDE", "HF VicRoads Internal", "VR Internal Stat", "VR Internal Loc", "NB_TYPE_SURVEY", "Start Time"]
+    names = ["Location", "CD_MELWAY", "NB_LATITUDE", "NB_LONGITUDE", "HF VicRoads Internal", "VR Internal Stat", "VR Internal Loc", "NB_TYPE_SURVEY", "Start Time"]
 
     cols = list(pd.read_csv("boroondara.csv", nrows=1))
     dftest = pd.read_csv("boroondara.csv", usecols=[i for i in cols if i not in names], encoding="utf-8").fillna(0)
     
-    print("printing iloc")
-    print(dftest.iloc[0])
+    #print("printing iloc") 
+
+    #there are 30 days (1 - 31), there are 4 entries? 
+    #train data can be 3*30 days, test data can be 1*30
     
+    currentRowIndex = 0 
+
+    #loop through all rows in the dataset
+    #while currentRowIndex != dftest.shape[0] :
+        #looping through the shit
+        #print(dftest.iloc[currentRowIndex]) #print the data
+        #currentRowIndex+=1  
+        
+    #extract some test data from df.iloc[0]
+
+
+    #load in the new train and test data
+
+    print("loading train data")
+    cols1 = list(pd.read_csv("boroondaraUpdatedTrain.csv", nrows=1))
+    print("hi")
+    dfTrainNew = pd.read_csv("boroondaraUpdatedTrain.csv", usecols=[i for i in cols1 if i not in names], encoding='utf-8').fillna(0)
+    print("loading test data")
+    cols2 = list(pd.read_csv("boroondaraUpdatedTest.csv", nrows=1))
+    print("hi")
+    dfTestNew = pd.read_csv("boroondaraUpdatedTest.csv", usecols=[i for i in cols2 if i not in names], encoding='utf-8').fillna(0)
+
+    print("printing new test data:")
+
+    print(dfTestNew.iloc[0])
 
     exit
+    currentRowIndex = 0
+    while currentRowIndex != dfTrainNew.shape[0] :
+        print(dfTrainNew.iloc[currentRowIndex])
+        currentRowIndex+=1
+
+    print("Printing new train data:")
+
+    currentRowIndex = 0
+
+    while currentRowIndex != dfTestNew.shape[0] : 
+        print(dfTestNew.iloc[currentRowIndex])
+        currentRowIndex+=1
+
+    #exit
 
     df2 = pd.read_csv(test, encoding='utf-8').fillna(0)
 
