4eabe5be9ce1f561121019a581ee16276f77da13
ChristianDeGroot <104880638+ChristianDeGroot@users.noreply.github.com>
2022-10-12 21:11:18 +1100

    can train models using boroondara.csv

--- data/data.py
+++ data/data.py
@@ -1,12 +1,132 @@
 """
 Processing the data
 """
+#from xml.etree.ElementTree import _FileRead
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
+def process_data_from_one_file(filename, lags):
 
-def process_data(train="train.csv", test="test.csv", lags=12):
+    #use the boorondara file and split it into train and test data
+
+    df1 = pd.read_csv(filename, encoding='utf-8').fillna(0)
+
+    #no idea what these 3 things are lolllll
+    #scaler = MinMaxScaler(feature_range=(0, 1)).fit(df1[attr].values.reshape(-1, 1))
+    #flow1 = scaler.transform(df1[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    #flow2 = scaler.transform(df2[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    
+    trainArray, testArray = [], []
+    trainArrayBigLongSingleColumn, testArrayBigLongSingleColumn = [], []
+
+    fileRowIndex = 0
+    columnIndexOfLocation = 1 #the 'location' column index 
+    
+    #make a new file with the dropped columns
+    dfDataOnly = df1.drop(['SCATS Number','Location' , 'CD_MELWAY' ,'NB_LATITUDE', 'NB_LONGITUDE', 'HF VicRoads Internal', 'VR Internal Stat', 'VR Internal Loc', 'NB_TYPE_SURVEY', 'Start Time'], axis='columns')
+    print("hi")
+    #df1.iloc[row, column] reads the entry
+    scanningFile = True
+    while fileRowIndex < df1.shape[0]: 
+        #first, get the amount of days in the scat site
+        days = 0
+        
+        currentSiteName = df1.iloc[fileRowIndex, columnIndexOfLocation]
+
+        print('site = ' + currentSiteName)
+        #loop forward until we find out how many days are recorded for this site
+        while df1.iloc[fileRowIndex + days, columnIndexOfLocation] == currentSiteName :
+            
+            days+=1
+            if fileRowIndex + days == df1.shape[0] - 1:
+                print("end!")
+                break 
+            
+
+        #print("days = " + str(days))
+        #split the data into 80% train and 20% test data
+        for i in range(0, days):
+            if i < round(days*0.8) : 
+                trainArray.append(dfDataOnly.iloc[fileRowIndex]) 
+                for b in range(0, 96):
+                    trainArrayBigLongSingleColumn.append(dfDataOnly.iloc[fileRowIndex, b]) 
+            else:  
+                testArray.append(dfDataOnly.iloc[fileRowIndex]) 
+                for b in range(0, 96):
+                    testArrayBigLongSingleColumn.append(dfDataOnly.iloc[fileRowIndex, b]) 
+            fileRowIndex+=1
+            if fileRowIndex == df1.shape[0] - 1:
+                fileRowIndex = df1.shape[0]
+        print(fileRowIndex)
+    print("done")
+    #print(trainArray) 
+
+    #convert to dataframe
+    newTrainDf = pd.DataFrame(trainArray)
+    newTestDf = pd.DataFrame(testArray)
+    trainDfSingle = pd.DataFrame(trainArrayBigLongSingleColumn)
+    testDfSingle = pd.DataFrame(testArrayBigLongSingleColumn)
+
+
+    print("printing new train df")
+    print(newTrainDf) 
+    print("printing new test df")
+    print(newTestDf) 
+
+    print("printing new train df in single column form")
+    print(trainDfSingle) 
+    print("printing new test df in single column form")
+    print(testDfSingle) 
+
+    dfbruh = pd.read_csv("data/train.csv", encoding='utf-8').fillna(0)
+    print("printing format of train.csv - df[attr]")
+    print(dfbruh['Lane 1 Flow (Veh/5 Minutes)'])
+    
+    
+    scaler1 = MinMaxScaler(feature_range=(0, 1)).fit(dfbruh['Lane 1 Flow (Veh/5 Minutes)'].values.reshape(-1, 1))
+    scaler2 = MinMaxScaler(feature_range=(0, 1)).fit(dfDataOnly.iloc[0].values.reshape(-1, 1))
+    print("scalar original: ")
+    print(scaler1)
+    print("new scaler")
+    print(scaler2)
+
+
+    print("original flow1")
+    flow1 = scaler1.transform(dfbruh['Lane 1 Flow (Veh/5 Minutes)'].values.reshape(-1, 1)).reshape(1, -1)[0]
+    print(flow1)
+
+    print("new flow1")
+
+    flow1New = scaler2.transform(trainDfSingle[0].values.reshape(-1, 1)).reshape(1, -1)[0]
+    flow2New = scaler2.transform(testDfSingle[0].values.reshape(-1, 1)).reshape(1, -1)[0]
+    print(flow1New)
+    print("new flow2")
+    print(flow2New)
+    
+    
+    train, test = [], []
+    for i in range(lags, len(flow1New)):
+        train.append(flow1New[i - lags: i + 1])
+    for i in range(lags, len(flow2New)):
+        test.append(flow2New[i - lags: i + 1])
+
+    train = np.array(train)
+    test = np.array(test)
+    np.random.shuffle(train)
+
+    X_train = train[:, :-1]
+    y_train = train[:, -1]
+    X_test = test[:, :-1]
+    y_test = test[:, -1]
+    
+
+    return X_train, y_train, X_test, y_test, scaler2
+
+def process_data(train, test, lags):
+    return process_data_from_one_file('data/boroondara.csv', lags)
+    print("done")
+    exit;
     """Process data
     Reshape and split train\test data.
 
@@ -22,74 +142,17 @@ def process_data(train="train.csv", test="test.csv", lags=12):
         scaler: StandardScaler.
     """
     attr = 'Lane 1 Flow (Veh/5 Minutes)'
-
-    
-
     df1 = pd.read_csv(train, encoding='utf-8').fillna(0)
-    #print("test")
-    #print(df1)
-    #print("printing df1[attr]")
-    #print(df1[attr])
-
-    names = ["Location", "CD_MELWAY", "NB_LATITUDE", "NB_LONGITUDE", "HF VicRoads Internal", "VR Internal Stat", "VR Internal Loc", "NB_TYPE_SURVEY", "Start Time"]
-
-    cols = list(pd.read_csv("boroondara.csv", nrows=1))
-    dftest = pd.read_csv("boroondara.csv", usecols=[i for i in cols if i not in names], encoding="utf-8").fillna(0)
-    
-    #print("printing iloc") 
-
-    #there are 30 days (1 - 31), there are 4 entries? 
-    #train data can be 3*30 days, test data can be 1*30
-    
-    currentRowIndex = 0 
-
-    #loop through all rows in the dataset
-    #while currentRowIndex != dftest.shape[0] :
-        #looping through the shit
-        #print(dftest.iloc[currentRowIndex]) #print the data
-        #currentRowIndex+=1  
-        
-    #extract some test data from df.iloc[0]
-
-
-    #load in the new train and test data
-
-    print("loading train data")
-    cols1 = list(pd.read_csv("boroondaraUpdatedTrain.csv", nrows=1))
-    print("hi")
-    dfTrainNew = pd.read_csv("boroondaraUpdatedTrain.csv", usecols=[i for i in cols1 if i not in names], encoding='utf-8').fillna(0)
-    print("loading test data")
-    cols2 = list(pd.read_csv("boroondaraUpdatedTest.csv", nrows=1))
-    print("hi")
-    dfTestNew = pd.read_csv("boroondaraUpdatedTest.csv", usecols=[i for i in cols2 if i not in names], encoding='utf-8').fillna(0)
-
-    print("printing new test data:")
-
-    print(dfTestNew.iloc[0])
-
-    exit
-    currentRowIndex = 0
-    while currentRowIndex != dfTrainNew.shape[0] :
-        print(dfTrainNew.iloc[currentRowIndex])
-        currentRowIndex+=1
-
-    print("Printing new train data:")
-
-    currentRowIndex = 0
-
-    while currentRowIndex != dfTestNew.shape[0] : 
-        print(dfTestNew.iloc[currentRowIndex])
-        currentRowIndex+=1
-
-    #exit
-
     df2 = pd.read_csv(test, encoding='utf-8').fillna(0)
 
     # scaler = StandardScaler().fit(df1[attr].values)
     scaler = MinMaxScaler(feature_range=(0, 1)).fit(df1[attr].values.reshape(-1, 1))
     flow1 = scaler.transform(df1[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
     flow2 = scaler.transform(df2[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
-
+    print("start")
+    print(flow1)
+    print("hi")
+    exit
     train, test = [], []
     for i in range(lags, len(flow1)):
         train.append(flow1[i - lags: i + 1])
@@ -106,4 +169,3 @@ def process_data(train="train.csv", test="test.csv", lags=12):
     y_test = test[:, -1]
 
     return X_train, y_train, X_test, y_test, scaler
-process_data()
