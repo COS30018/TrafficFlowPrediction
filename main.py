8dd9de247b29160869fb8414f7b01b2f5d5972f7
ChristianDeGroot <104880638+ChristianDeGroot@users.noreply.github.com>
2022-10-20 16:30:25 +1100

    gui

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import numpy as np
 import pandas as pd
 from data.data import process_data
 from keras.models import load_model
-from keras.utils.vis_utils import plot_model
+from keras.utils.vis_utils import plot_model 
 import sklearn.metrics as metrics
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import tkinter as tk
+from PIL import Image, ImageTk
+
 warnings.filterwarnings("ignore")
 
 
@@ -92,8 +95,45 @@ def plot_results(y_true, y_preds, names):
 
     plt.show()
 
+#easier code to create a circle
+def _create_circle(self, x, y, r, **kwargs):
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.create_circle = _create_circle
+
+def mousePress(event):
+   x= event.x
+   y= event.y
+   print("Pointer is currently at %d, %d" %(x,y))
+    
+
 
 def main():
+
+    root = tk.Tk() 
+    
+    canvas = tk.Canvas(root, width = 1000, height = 500)      
+    
+    sites = [canvas]
+    img1 = canvas.create_rectangle(50, 50, 50, 50, fill="red", outline = 'blue') 
+    sites.append(img1) 
+    canvas.bind("<Button-1>", mousePress) 
+
+    #canvas.pack()      #make canvas visible
+    img = tk.PhotoImage(file="images/dog.png")      #load image
+    canvas.create_image(20,20, anchor=tk.NW, image=img) #create image on canvas
+    
+    canvas.create_circle(20, 20, 20, fill='white', outline="black", width=2) #draws a filled circle
+    canvas.create_circle(160, 120, 80, outline="red", width=2) #draws the circle around the dog
+    
+    abs_coord_x = root.winfo_pointerx() - root.winfo_rootx()
+    abs_coord_y = root.winfo_pointery() - root.winfo_rooty()
+    
+    while True:
+        canvas.move(img1, 0.001,0) #move img1 by 0.001 every update, img1 is the small square
+        root.update()
+        canvas.pack()
+    #root.mainloop()  
+    """ 
     lstm = load_model('model/lstm.h5')
     gru = load_model('model/gru.h5')
     saes = load_model('model/saes.h5')
@@ -121,6 +161,7 @@ def main():
         eva_regress(y_test, predicted)
 
     plot_results(y_test[: 96], y_preds, names)
+    """
 
 
 if __name__ == '__main__':
