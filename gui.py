a3d8287c9763df9a04886df7bc5b8cdd4b4d6d0c
Harry Sheppard <harry.99lives@pm.me>
2022-10-25 13:40:41 +1100

    gui now uses tk Frames

--- gui.py
+++ gui.py
@@ -1,23 +1,21 @@
+from doctest import master
 from enum import unique
 from re import sub
-import tkinter
+import tkinter as tk
 import tkintermapview
 import pandas as pd
 
 def main():
     
-    # Create and start map at Boroondora
-    window_w = 1600
-    window_h = 1200
-
-    root = tkinter.Tk()
-    root.geometry(f"{window_w}x{window_h}")
+    # Set up root window
+    root = tk.Tk()
     
-    map_widget = tkintermapview.TkinterMapView(root, width=window_w, height=window_h, corner_radius=0)
-    map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-    
-    map_widget.set_position(-37.81157, 145.00978)  # Paris, France
-    map_widget.set_zoom(15)
+    # Set up map
+    frame_map = tk.Frame()
+    map_widget = tkintermapview.TkinterMapView(frame_map, width=1600, height=1200, corner_radius=0)
+    map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    map_widget.set_position(-37.8236299, 145.0699878)  # Paris, France
+    map_widget.set_zoom(14)
     
     # Mark SCATS
     data_df = pd.read_csv("data/boroondara.csv")
@@ -25,8 +23,19 @@ def main():
     markers = {}
     for _, row in unique_sites_df.iterrows():
         new_marker = map_widget.set_marker(row['NB_LATITUDE'], row['NB_LONGITUDE'])
+        #new_marker.set_text("SCATS: "+str(row['SCATS Number'])+"\nLocation: "+row['Location'])
         markers[row['SCATS Number']] = {row['Location'] : new_marker}
+        
+    # Set up menu
+    frame_menu = tk.Frame()
+    menu = tk.Label(master=frame_menu, text="MENU HERE")
+    menu.pack()
     
+
+    map_widget.pack()
+    frame_map.pack()
+    frame_menu.pack()
+
     root.mainloop()
     
 
