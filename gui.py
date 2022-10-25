423debf389eb977dac566e015414bef467913b4d
Harry Sheppard <harry.99lives@pm.me>
2022-10-25 15:57:38 +1100

    Input boxes for start and destination SCATS

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ from doctest import master
 from enum import unique
 from re import sub
 import tkinter as tk
+from tkinter import ttk
 import tkintermapview
 import pandas as pd
 
@@ -12,7 +13,8 @@ class MapGUI(tk.Tk):
         
         # Get Dataframe of boroondara data
         data_df = pd.read_csv("data/boroondara.csv")
-    
+        self.unique_scats = list(data_df.groupby(['SCATS Number']).groups)
+        
         # Set up root window
         tk.Tk.__init__(self, *args, **kwargs)
         
@@ -23,7 +25,7 @@ class MapGUI(tk.Tk):
         self.map_widget.set_position(-37.8236299, 145.0699878)  # Paris, France
         self.map_widget.set_zoom(14)
         
-        # Mark SCATS
+        ## Mark SCATS
         unique_sites_df = data_df.drop_duplicates(subset=['SCATS Number', 'Location'])
         self.markers = {}
         for _, row in unique_sites_df.iterrows():
@@ -36,12 +38,18 @@ class MapGUI(tk.Tk):
     
         # Set up menu
         self.frame_menu = tk.Frame()
-        self.menu = tk.Label(master=self.frame_menu, text="MENU HERE")
-        self.menu.pack()
+        ## Starting SCATS input
+        self.dropdown_start_selected = tk.StringVar()
+        self.dropdown_start = ttk.Combobox(master=self.frame_menu, textvariable=self.dropdown_start_selected, values=self.unique_scats, state='readonly')
+        self.dropdown_start.pack(side='left', padx=20, pady=20)
+        ## Destination SCATS input
+        self.dropdown_dest_selected = tk.StringVar()
+        self.dropdown_dest = ttk.Combobox(master=self.frame_menu, textvariable=self.dropdown_dest_selected, values=self.unique_scats, state='readonly')
+        self.dropdown_dest.pack(padx=20, pady=20)
         
     
         self.frame_map.pack()
-        self.frame_menu.pack()
+        self.frame_menu.pack(side="bottom")
     
     def start(self):
         self.mainloop()
