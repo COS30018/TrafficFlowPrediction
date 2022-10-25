1dd335ed76c9147bdf1abda666bdb934d1a7ae95
Harry Sheppard <harry.99lives@pm.me>
2022-10-25 14:22:15 +1100

    Converted app to class

--- gui.py
+++ gui.py
@@ -5,39 +5,48 @@ import tkinter as tk
 import tkintermapview
 import pandas as pd
 
-def main():
-    
-    # Set up root window
-    root = tk.Tk()
-    
-    # Set up map
-    frame_map = tk.Frame()
-    map_widget = tkintermapview.TkinterMapView(frame_map, width=1600, height=1200, corner_radius=0)
-    map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-    map_widget.set_position(-37.8236299, 145.0699878)  # Paris, France
-    map_widget.set_zoom(14)
-    
-    # Mark SCATS
-    data_df = pd.read_csv("data/boroondara.csv")
-    unique_sites_df = data_df.drop_duplicates(subset=['SCATS Number', 'Location'])
-    markers = {}
-    for _, row in unique_sites_df.iterrows():
-        new_marker = map_widget.set_marker(row['NB_LATITUDE'], row['NB_LONGITUDE'])
-        #new_marker.set_text("SCATS: "+str(row['SCATS Number'])+"\nLocation: "+row['Location'])
-        markers[row['SCATS Number']] = {row['Location'] : new_marker}
+
+class MapGUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
         
-    # Set up menu
-    frame_menu = tk.Frame()
-    menu = tk.Label(master=frame_menu, text="MENU HERE")
-    menu.pack()
+        # Get Dataframe of boroondara data
+        data_df = pd.read_csv("data/boroondara.csv")
     
-
-    map_widget.pack()
-    frame_map.pack()
-    frame_menu.pack()
-
-    root.mainloop()
+        # Set up root window
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        # Set up map
+        self.frame_map = tk.Frame()
+        self.map_widget = tkintermapview.TkinterMapView(self.frame_map, width=1600, height=1200, corner_radius=0)
+        self.map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.map_widget.set_position(-37.8236299, 145.0699878)  # Paris, France
+        self.map_widget.set_zoom(14)
+        
+        # Mark SCATS
+        unique_sites_df = data_df.drop_duplicates(subset=['SCATS Number', 'Location'])
+        self.markers = {}
+        for _, row in unique_sites_df.iterrows():
+            new_marker = self.map_widget.set_marker(row['NB_LATITUDE'], row['NB_LONGITUDE'])
+            #new_marker.set_text("SCATS: "+str(row['SCATS Number'])+"\nLocation: "+row['Location'])
+            self.markers[row['SCATS Number']] = {row['Location'] : new_marker}
+            
+        # Pack map widget
+        self.map_widget.pack()
     
+        # Set up menu
+        self.frame_menu = tk.Frame()
+        self.menu = tk.Label(master=self.frame_menu, text="MENU HERE")
+        self.menu.pack()
+        
+    
+        self.frame_map.pack()
+        self.frame_menu.pack()
+    
+    def start(self):
+        self.mainloop()
+        
 
 if __name__ == '__main__':
-    main()
+    gui = MapGUI()
+    gui.start()
