f615cec4e5a24aa5fb5299261a6cfbd6e6d3e958
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-10-28 16:49:33 +1100

    Update graph_search.py

--- graph_search.py
+++ graph_search.py
@@ -1,13 +1,43 @@
+from pyexpat import model
+import math 
 import geopy.distance
 from map_parser import parse_csv
-DEFAULT_SPEED = 60
+from predict import precict,process_data, load_model_file
+from datetime import datetime, timedelta
+
+MAX_SPEED = 60
+
+def round_dt(dt, delta=timedelta(minutes=15)):
+    return datetime.min + round((dt - datetime.min) / delta) * delta
+
+def flow_to_speed(flow):
+    flow = int(flow)
+    speed = (62.5 + math.sqrt(3906.25 + 3.908*int(flow)))/2.153125
+    print(speed)
+    if(speed>MAX_SPEED):
+        speed = MAX_SPEED
+    elif(speed<5):
+        speed = 5
+    
+    
+    return speed
+class Model:
+    def __init__(self,model_number,model):
+        self.model_number = model_number
+        self.model = model
+    def is_model(self,model_number):
+        if(str(self.model_number) == str(model_number)):
+            return True
+        
+        return False
+    
 
 class Node:
-    def __init__(self, scats, parent=None):
+    def __init__(self, scats, parent=None,):
         self.scats = scats
         self.parent = parent
-        self.cost = self.calculate_cost()
         self.heuristic_value = 0
+        self.cost = 0
         self.traversed_scats = self.initiate_traversed_scats_list()
         
     def initiate_traversed_scats_list(self):
@@ -32,19 +62,24 @@ class Node:
         child_nodes = []
         for other_scats in self.scats.connections:
             if(not self.went_through_scats(other_scats)):
-                child_node = Node(other_scats,self)
+                child_node = Node(other_scats,parent=self)
                 child_node.add_traversed_scats(self.scats)
                 child_nodes.append(child_node)
                 
         return child_nodes
     
-    def calculate_cost(self,default_cost=30):
+    def calculate_cost(self, data_df, scaler, model, default_cost=30):
         cost = default_cost
         if(self.parent is None):
             return cost
         #calculate distance in km using geopy
         distance_from_parent = self.distance_from(self.parent.scats)
-        cost += (float)(distance_from_parent/DEFAULT_SPEED)*3600
+        
+        #predict the traffic flow
+        traffic_flow = precict(self.scats.number,round_dt(datetime.now()),data_df=data_df,scaler=scaler,model=model)
+        speed = flow_to_speed(traffic_flow)
+        
+        cost += (float)(distance_from_parent/speed)*3600
         
         cost += self.parent.cost
         
@@ -73,12 +108,16 @@ class Node:
     
         
 class SearchAStar:
-    def __init__(self, solution_size=5):
+    def __init__(self, data_df, scaler,solution_size=5):
         self.frontier = []
         self.solution_size = solution_size
+        self.data_df = data_df
+        self.scaler = scaler
+        self.models = []
         
     def add_to_frontier(self,node):
         self.frontier.append(node)
+    
     
     def pop_frontier(self):
         return self.frontier.pop()
@@ -86,7 +125,7 @@ class SearchAStar:
     def search(self,start_scats, end_scats):
         solutions = []
         #first add the start node to frontier
-        self.add_to_frontier(Node(start_scats))
+        self.add_to_frontier(Node(scats=start_scats))
         
         #loop over the frontier
         while(len(self.frontier)>0):
@@ -104,6 +143,11 @@ class SearchAStar:
             #if not the goal, explore this node
             child_nodes = current_node.explore_available_children()
             for child_node in child_nodes:
+                #calculate cost
+                
+                cost = child_node.calculate_cost(data_df=self.data_df, scaler=self.scaler,model=self.get_model(child_node.scats.number).model)
+                child_node.set_cost(cost)
+                
                 #calculate heuristic function
                 heuristic_value = self.calculate_heuristic(child_node,end_scats)
                 child_node.set_heuristic_value(heuristic_value)
@@ -114,11 +158,20 @@ class SearchAStar:
             self.frontier.sort(key=lambda x: x.heuristic_value,reverse=True)
             
         return solutions
-    
+    def get_model(self,scats_number):
+
+        for model in self.models:
+            if(model.is_model(scats_number)):
+                return model
+        
+        new_model = Model(scats_number,load_model_file('lstm',scats_number))
+        self.models.append(new_model)
+        return new_model
     def calculate_heuristic(self, node, end_scats):
         #f = g + h
-        g = node.cost 
-        h = node.distance_from(end_scats)/DEFAULT_SPEED*3600
+        g = node.cost
+        
+        h = node.distance_from(end_scats)/MAX_SPEED*3600
         
         f = g + h
         
@@ -152,7 +205,10 @@ def print_solution(solution):
 def main():
     scats_list = parse_csv()
     
-    A_star_search = SearchAStar()
+    data_df, scaler = process_data()
+    
+    
+    A_star_search = SearchAStar(data_df=data_df,scaler=scaler)
     
     start_num = int(input('Enter start point: '))
     
